f43304187cab5819647b319a77a7a809128e1da5
Marinescu Mircea Valentin <marinescumirceavalentin@gmail.com>
2025-06-21 21:42:31 +0300

    update image routing

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,34 +1,37 @@
+import calcImg  from '../assets/calculator.png';
+import snakeImg from '../assets/snake.png';
+import assemImg  from '../assets/assembly.png';
+import chatImg from '../assets/chatroom.png';
+
 const projects = [
   {
     id: 1,
     title: 'Dynamic iPhone Style Calculator',
-    image: 'src/assets/calculator.png',
+    image: calcImg,
     description: 'An iPhone-style calculator built with JS and HTML/CSS.',
     link: '/projects/ios-calculator/calculator.html'
   },
   {
     id: 2,
     title: 'Snake Game (HTML/CSS/JS)',
-    image: 'src/assets/snake.png',
+    image: snakeImg,
     description: 'Classic Snake game made with vanilla JS.',
     link: '/projects/snake-game/snake.html'
   },
   {
     id: 3,
     title: 'Python3 Chatroom w/Sockets',
-    image: 'src/assets/chatroom.png',
+    image: chatImg,
     description: 'Small chatroom using Python3 sockets. Tutorial to run and use the code for this project is available below!',
     link: 'hhttps://github.com/mirceavalentin/Python3-Chatroom'
   },
   {
     id: 4,
     title: 'Small x86-64 AT&T Assembly Projects',
-    image: 'src/assets/assembly.png',
+    image: assemImg,
     description: 'These include a Factorial, Intrement, Power and a simplifed Unix "diff" program.',
     link: 'https://github.com/mirceavalentin/Assembly-Programs'
   },
-
-  // …more projects
 ];
 
 export default projects;
